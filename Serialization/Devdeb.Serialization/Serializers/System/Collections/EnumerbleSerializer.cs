5088da3d721ed416f78d32ac53c41b18d6b32de0
alexsey chuvilkin <leha.chuvilkin.90@mail.ru>
2021-01-29 00:18:44 +0300

    Default serializer (serialization info of selected member ).

---
+++ Serialization/Devdeb.Serialization/Serializers/System/Collections/EnumerbleSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Devdeb.Serialization.Serializers.System.Collections
+{
+	public sealed class EnumerbleSerializer<T> : Serializer<IEnumerable<T>>
+	{
+		private readonly ISerializer<T> _elementSerializer;
+
+		public EnumerbleSerializer(ISerializer<T> elementSerializer) : base(SerializerFlags.NeedCount)
+		{
+			_elementSerializer = elementSerializer ?? throw new ArgumentNullException(nameof(elementSerializer));
+		}
+
+		public override int Size(IEnumerable<T> instance)
+		{
+			VerifySize(instance);
+			int size = 0;
+			foreach(T element in instance)
+				size += _elementSerializer.Size(element);
+			return size;
+		}
+		public override void Serialize(IEnumerable<T> instance, byte[] buffer, int offset)
+		{
+			VerifySerialize(instance, buffer, offset);
+			foreach(T element in instance)
+				_elementSerializer.Serialize(element, buffer, ref offset);
+		}
+		public override IEnumerable<T> Deserialize(byte[] buffer, int offset, int? count = null)
+		{
+			VerifyDeserialize(buffer, offset, count);
+			T[] instance = new T[count.Value];
+			for (int i = 0; i != instance.Length; i++)
+				instance[i] = _elementSerializer.Deserialize(buffer, ref offset, null);
+			return instance;
+		}
+	}
+}
--- Serialization/Tests/Devdeb.Serialization.Tests/Program.cs
+++ Serialization/Tests/Devdeb.Serialization.Tests/Program.cs
@@ -1,16 +1,16 @@
-﻿using System;
-
-namespace Devdeb.Serialization.Tests
+﻿namespace Devdeb.Serialization.Tests
 {
 	class Program
 	{
 		private static readonly SerializationTest _serializationTest;
+		private static readonly DefaultSerializationTest _defaultSerializationTest;
 
 		static unsafe Program()
 		{
 			_serializationTest = new SerializationTest();
+			_defaultSerializationTest = new DefaultSerializationTest();
 		}
 
-		static private void Main(string[] args) => _serializationTest.Test();
+		static private void Main(string[] args) => _defaultSerializationTest.Test();
 	}
 }
